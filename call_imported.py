2b134749176356661ab4a779f6f4ae3e28a42a4c
BiancaMass <b.massacci@student.maastrichtuniversity.nl>
2024-03-01 14:21:57 +0100

    Initial commit

--- call_imported.py
+++ call_imported.py
@@ -8,8 +8,8 @@ current_time = datetime.now()
 string_time = current_time.strftime("%Y-%m-%d-%H%M")
 output_dir = os.path.join(f"./output/0_imported_circuits/{string_time}")
 
-qasm_file_path= "/Users/bmassacci/main_folder/maastricht/academics/quantum_thesis/scripts/PQWGAN" \
-                "/input/final_best_ciruit_maximize_3.qasm"
+qasm_file_path= "/Users/bmassacci/main_folder/maastricht/academics/quantum_thesis/scripts/B-WGAN" \
+                "-Evol/input/final_best_ciruit_maximize_3.qasm"
 
 if __name__ == "__main__":
     train_imported(classes_str="01",
